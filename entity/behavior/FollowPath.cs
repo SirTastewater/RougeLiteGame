075186c4c00e79337a97c53b8bf847c1f79b2762
Ivo Quiring <falk.quiring@gmail.com>
2025-10-01 11:38:31 +0200

    Implement basic path follow

---
+++ entity/behavior/FollowPath.cs
@@ -0,0 +1,36 @@
+using Godot;
+
+namespace RougeLiteGame.entity.behavior;
+
+[GlobalClass] public partial class FollowPath : IdleBehavior
+{
+    
+    [Export(PropertyHint.NodePathValidTypes, "PathFollow3D")] private NodePath _path;
+    private PathFollow3D _path3D;
+    [Export(PropertyHint.Range, "1,30")]
+    private float _samplePathPointDistance = 5;
+
+    protected override void Init()
+    {
+        _path3D = GetNode<PathFollow3D>(_path);
+        SetTargetPosition(_path3D.GlobalPosition);
+        base.Init();
+    }
+
+    public override Vector3 Process(double delta)
+    {
+        if (!IsFacingTarget())
+        {
+            RotateTowards(delta);
+            return Vector3.Zero;
+        }
+        
+        if (!Controller.IsNavigationFinished()) return FollowControllerTarget();
+        
+        _path3D.Progress += _samplePathPointDistance;
+        Vector3 targetPosition = _path3D.GlobalPosition;
+        SetTargetPosition(targetPosition);
+
+        return FollowControllerTarget();
+    }
+}
--- entity/behavior/StatueIdleBehavior.cs
+++ entity/behavior/StatueIdleBehavior.cs
@@ -7,7 +7,6 @@ namespace RougeLiteGame.entity.behavior;
 [GlobalClass] public partial class StatueIdleBehavior : IdleBehavior
 {
     private Vector3 _targetPosition;
-    private bool _isFacingTarget;
 
     protected override void Init()
     {
@@ -20,29 +19,9 @@ namespace RougeLiteGame.entity.behavior;
 
         if (GetTargetPosition() != Vector3.Zero && !Controller.IsNavigationFinished())
         {
-            if (_isFacingTarget) return FollowControllerTarget();
+            if (IsFacingTarget()) return FollowControllerTarget();
             
-            Vector3 target = GetTargetPosition() - Entity.GlobalPosition;
-            target.Y = 0;
-            target.Normalized();
-                
-            Vector3 facing = -Entity.GlobalTransform.Basis.Z;
-            facing.Y = 0;
-            facing = facing.Normalized();
-
-            float angle = facing.AngleTo(target);
-
-            if (angle < 0.01f)
-            {
-                _isFacingTarget = true;
-                return FollowControllerTarget();
-            }
-                
-            float cross = facing.Cross(target).Y;
-            float turnDirection = float.Sign(cross);
-            float rotationAmount = Mathf.Min(angle, 2.0f * (float) delta);
-            
-            Entity.Rotation = new Vector3(Entity.Rotation.X, Entity.Rotation.Y + turnDirection * rotationAmount, Entity.Rotation.Z);
+            RotateTowards(delta);
             return Vector3.Zero;
         }
         
@@ -59,7 +38,6 @@ namespace RougeLiteGame.entity.behavior;
         } while(randomPosition.DistanceTo(Entity.GlobalPosition) < 4);
             
         SetTargetPosition(randomPosition);
-        _isFacingTarget = false;
         
         return randomPosition;
     }
