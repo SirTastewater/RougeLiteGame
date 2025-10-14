e14d46732985ae035fed72e2afff4321b5b0dd0b
Ivo Quiring <falk.quiring@gmail.com>
2025-10-14 21:43:42 +0200

    Add functions to add limbs

--- entity/Entity.cs
+++ entity/Entity.cs
@@ -1,7 +1,9 @@
+using System;
 using Godot;
 using Godot.Collections;
 using RougeLiteGame.entity.limbs;
 using RougeLiteGame.entity.limbs.instance;
+using RougeLiteGame.logger;
 
 namespace RougeLiteGame.entity;
 
@@ -15,6 +17,8 @@ namespace RougeLiteGame.entity;
 [GlobalClass]
 public abstract partial class Entity : CharacterBody3D
 {
+    private readonly static ILogger Logger = LoggerFactory.GetLogger<Entity>();
+    
     [Export] private EntityController _entityController;
     private BodyStructure _bodyStructure;
     
@@ -28,10 +32,40 @@ public abstract partial class Entity : CharacterBody3D
     {
         base._Ready();
 
+        UpdateStructure();
         _entityController?.Connect(this);
-        _bodyStructure = new BodyStructure(_torso, _headLimbs, _armLimbs, _legLimbs);
     }
 
+    public void SetTorso(TorsoInstance torso)
+    {
+        _torso = torso;
+        UpdateStructure();
+    }
+    
+    public void AddHead(HeadInstance head)
+    {
+        _headLimbs.Add(head);
+        UpdateStructure();
+    }
+    
+    public void AddArm(ArmInstance arm)
+    {
+        _armLimbs.Add(arm);
+        UpdateStructure();
+    }
+    
+    public void AddLeg(LegInstance leg)
+    {
+        _legLimbs.Add(leg);
+        UpdateStructure();
+    }
+
+    private void UpdateStructure()
+    {
+        Logger.Trace("{}: Updating body structure.", GetName());
+        _bodyStructure = new BodyStructure(_torso, _headLimbs, _armLimbs, _legLimbs);
+    }
+    
     public BodyStructure GetLimbs()
     {
         return _bodyStructure;
