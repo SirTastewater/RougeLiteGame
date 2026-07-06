2eb1791527641122aec45ef2dc11afae9cefaac5
Ivo Quiring <falk.quiring@gmail.com>
2026-07-06 19:15:02 +0200

    Updated stuff

--- environment/rooms/RoomLibraryGenerator.cs
+++ environment/rooms/RoomLibraryGenerator.cs
@@ -7,7 +7,7 @@ namespace RougeLiteGame.environment.rooms;
 [Tool]
 public partial class RoomLibraryGenerator : EditorScenePostImport
 {
-    private static readonly ILogger Logger = LoggerFactory.GetLogger<RoomLibraryGenerator>();
+    private static readonly ILogger Logger = LoggerFactory.GetLogger<RoomLibraryGenerator>(false);
     
     public override GodotObject _PostImport(Node scene)
     {
@@ -20,9 +20,11 @@ public partial class RoomLibraryGenerator : EditorScenePostImport
             RegisterEntry(i, children[i], library);
         }
 
-        const string path = "environment/rooms/RoomLibrary.meshlib";
+        const string path = "res://environment/rooms/RoomLibrary.meshlib";
         Logger.Info("Save library file in path {}.", path);
-        ResourceSaver.Save(library, path, ResourceSaver.SaverFlags.RelativePaths);
+        ResourceSaver.Save(library, path);
+        
+        Logger.Flush();
 
         return scene;
     }
@@ -30,15 +32,17 @@ public partial class RoomLibraryGenerator : EditorScenePostImport
     private static void RegisterEntry(int index, Node child, MeshLibrary library)
     {
         Logger.Trace("Register {} as asset in the mesh library", child.Name);
+        if (child is not MeshInstance3D mesh || mesh.Mesh == null) { return; }
         library.CreateItem(index);
-        if (child is not MeshInstance3D meshInstance) { return; }
-        library.SetItemMesh(index, meshInstance.GetMesh());
+        library.SetItemMesh(index, mesh.GetMesh());
         library.SetItemName(index, $"[{index}] {child.Name}");
-            
-        StaticBody3D staticBody3D = meshInstance.GetChild(0) as StaticBody3D;
+        
+        mesh.CreateTrimeshCollision(); // generate collision shape
+        
+        StaticBody3D staticBody3D = mesh.GetChild(0) as StaticBody3D;
         if (staticBody3D?.GetChild(0) is not CollisionShape3D shape) return;
         
-        Logger.Trace("Adding shape to asset {}.", child.Name);
-        library.SetItemShapes(index, [shape]);
+        Logger.Trace("Adding shape {} to asset {}.", shape.Shape, child.Name);
+        library.SetItemShapes(index, [shape.Shape]);
     }
 }
